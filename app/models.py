422b92f7f3ea1df28484f1d444ffffdbb4891984
Simon Heuschkel <simon.heuschkel@icloud.com>
2020-04-04 12:13:19 +0200

    @add new ingredients with plus button

--- app/models.py
+++ app/models.py
@@ -18,7 +18,7 @@ class Ingredient(db.Model):
     key = db.Column(Integer, primary_key=True)
     cocktail_key = db.Column(Integer, ForeignKey('cocktail.key'))
     name = db.Column(String, index=True)
-    quantity = db.Column(Float, index=True)
+    quantity = db.Column(String, index=True)
 
     def __repr__(self):
         return '<{}: {} of {}>'.format(self.name, self.quantity, self.cocktail_key)
--- app/static/js/app.js
+++ app/static/js/app.js
@@ -18,7 +18,32 @@ window.addEventListener('load', function () {
 
     let all_ings = document.querySelectorAll('.ing_wrapper');
     for (let ing of all_ings) {
-        ing.style.visibility = "hidden";
+        ing.style.display = "None";
+    }
+
+    let plus_sign = document.getElementById('plus_sign');
+    // plus_sign.style.display = 'block';
+    plus_sign.onclick = add_field;
+
+    function add_field() {
+        let ing_array = [];
+        console.log('add');
+        let ing_fields = document.querySelectorAll('.ing_wrapper');
+        for (let ing of ing_fields){
+            if (ing.style.display === 'none'){
+                ing_array.push(ing)
+            }
+        }
+        console.log(ing_array);
+
+        let new_field = ing_array.shift();
+        new_field.style.display = 'block';
+
+        console.log(ing_array.length);
+        if (ing_array.length === 1 ){
+            plus_sign.style.display = 'None';
+            plus_sign.disabled = 'disabled';
+        }
     }
 
 });
